03c2ec82c3c21daeaee4072497106f2797e9a015
Steven Nava <trey.nobles@gmail.com>
2024-05-13 10:16:18 -0400

    Added Practical_C and projects for chapter 1. Updated README.

---
+++ Practical_C/print_numbers.c
@@ -0,0 +1,10 @@
+// Trivial program to print out numbers from for loop
+#include <stdio.h>
+
+int main() {
+	int i;
+	for (i = 1; i <= 10; i++) {
+		printf("The current number is: %d\n", i);
+	}
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # C_Projects
 A repository for all of my C Projects
+
+Book 1: Practical C by Steve Oualline
+Chapter 1: Created hello_world and print_numbers as trivial projects.
