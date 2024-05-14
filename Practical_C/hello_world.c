78cee2012e0e10b21cf12805bad6fffa803c3c90
Steven Nava <trey.nobles@gmail.com>
2024-05-13 20:24:33 -0400

    Added comments to programs hello_world and print_numbers. Cleaned up print_numbers.

--- Practical_C/hello_world.c
+++ Practical_C/hello_world.c
@@ -1,6 +1,21 @@
+/*****************************************\
+*					  * 
+*  Name:   Hello World                    *
+*					  *
+*  Author:  Steven Nava 		  *
+*					  *
+*  Purpose: Trivial program to show	  *
+*           principles of C programming	  *
+*					  *
+*  Usage:   Run program and it prints	  *
+*           "hello world!"		  *
+*           				  *
+\*****************************************/
+
 #include <stdio.h>
 
 int main() {
+	/* Print "hello world!" */
 	printf("hello world!");
 	return(0);
 }
--- Practical_C/print_numbers.c
+++ Practical_C/print_numbers.c
@@ -1,9 +1,23 @@
-// Trivial program to print out numbers from for loop
+/********************************************************\
+ *							*
+ *  Name:    Print Numbers				*
+ *							*
+ *  Author:  Steven Nava				*
+ *  							*
+ *  Purpose: Trivial program to show for loop and print	*
+ *   	     in C.					*
+ *							*
+ *  Usage:   Run program and numbers 1 - 10 will print	*
+ *   	     out with the sentence "The current number	*
+ *   	     is: "					*
+ *							*
+\********************************************************/
+
 #include <stdio.h>
 
 int main() {
-	int i;
-	for (i = 1; i <= 10; i++) {
+	/* Prints out "The current number is: [1 - 10]" */
+	for (int i = 1; i <= 10; i++) {
 		printf("The current number is: %d\n", i);
 	}
 	return 0;
