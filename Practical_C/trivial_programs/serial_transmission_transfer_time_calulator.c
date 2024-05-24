001557ce0df78cab91740e286cda36742ef490c3
Steven Nava <trey.nobles@gmail.com>
2024-05-23 23:23:04 -0400

    Added trivial program for calculating the length of time it would take to transfer a file of a given size through a serial transmission line.

---
+++ Practical_C/trivial_programs/serial_transmission_transfer_time_calulator.c
@@ -0,0 +1,28 @@
+/********************************************************************************
+*										*
+*	Name:	 Serial Transmission Line Transfer Time Calculator		*
+*										*
+*	Author:	 Steven Nava							*
+*										*
+*	Purpose: A trivial program to calulate how long it would take to	*
+*		 transfer a file of a given size using a serial trasmission	*
+*		 line.								*
+*		 								*
+*	Usage:	 Run program and enter in a size and unit of measurement.	*
+*		 Program will calulate how long it would take to transfer	*
+*		 a file of given size at max speed through a serial 		*
+*		 transmission line.						*
+*										*
+*	Notes:	 A serial tramsmission line can transfer 960 characters		*
+*		 per minute. We will assume that 1 character equals 1 byte.	*
+*										*
+*********************************************************************************/
+
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+	/* variable declaration */
+
+	return 0;
+}
