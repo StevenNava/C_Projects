2a6031f5b45954d91c194f50472ea3e5b34089c7
Steven Nava <trey.nobles@gmail.com>
2024-05-23 23:35:20 -0400

    Added functionality to check for valid sizes and units of measure to serial transmission line calculator

--- Practical_C/trivial_programs/serial_transmission_transfer_time_calulator.c
+++ Practical_C/trivial_programs/serial_transmission_transfer_time_calulator.c
@@ -23,6 +23,42 @@
 
 int main() {
 	/* variable declaration */
+	char line[100],
+	invalid_characters[100],
+	unit_of_measure[5];
+	int number_read_successfully,
+	size_of_file,
+	size_of_file_in_bytes;
+	double length_of_time_to_transfer_file;
+
+	/* prompt and read in file size */
+	printf("Enter file size in whole numbers only: ");
+	fgets(line, sizeof(line), stdin);
+	number_read_successfully = sscanf(line, "%d", &size_of_file);
+	sscanf(line, "%s", &invalid_characters);
+
+	while(number_read_successfully != 1) {
+		printf("\nA valid whole number for file size was not entered in. Please try again.\n");
+		printf("Enter file size in whole numbers only: ");
+		fgets(line, sizeof(line), stdin);
+		number_read_successfully = sscanf(line, "%d", &size_of_file);
+		sscanf(line, "%s", &invalid_characters);
+	}
+
+	/* prompt user and read in unit of measurement */
+	printf("Enter unit of measurement -- it must be either 'byetes', KB, MB, or GB: ");
+	fgets(line, sizeof(line), stdin);
+	
+	/* check for valid units of measurement */
+	if(strcmp(line, "bytes") == 0) {
+		printf("\nUnit of measurement entered in is bytes.\n");
+	} else if(strcmp(line, "KB") == 0) {
+		printf("\nUnit of measurement entered in is kilobytes.\n");
+	} else if(strcmp(line, "MB") == 0) {
+		printf("\nUnit of measurement entered in is megabytes.\n");
+	} else if(strcmp(line, "GB") == 0) {
+		printf("\nUnit of measurement entered in is gigabytes.\n");
+	}
 
 	return 0;
 }
