9bf4ce58c93cecd5e3f11c1029dbbb82a8567f37
Steven Nava <trey.nobles@gmail.com>
2024-05-29 18:55:43 -0400

    Renamed variables and deleted unused code to clean up program

--- Practical_C/trivial_programs/count_positive_negative.c
+++ Practical_C/trivial_programs/count_positive_negative.c
@@ -31,58 +31,60 @@ int main() {
 	negative_number_count = 0,
 	all_numbers[100],
 	positive_numbers[50],
-	negative_numbers[50];
+	negative_numbers[50],
+	positive_number_incrementor = 0,
+	negative_number_incrementor = 0,
+	all_number_incrementor = 0,
+	number_string_incrementor = 0;
 	bool all_numbers_read_successfully = true;
 
 	/* prompt for numbers and read in values */
 	printf("Enter in a list of numbers with spaces between them: ");
 	fgets(line, sizeof(line), stdin);
 	
-	int l = 0;
+	int l = 0; // number string incrementor
 	int k = 0;
 	int j = 0;
 	int m = 0;
 	/* for every character aside from ending character */
 	for(int i = 0; i < strlen(line); i++) {
 		if(line[i] != ' ' && line[i] != '\n') {
-			number_string[l] = line[i];
-			l++;
+			number_string[number_string_incrementor] = line[i];
+			number_string_incrementor++;
 			continue;
 		}
 		
-		number_string[l] = '\0';
-		l = 0;
-		number_read_successfully = sscanf(number_string, "%d", &all_numbers[k]);
+		number_string[number_string_incrementor] = '\0';
+		number_string_incrementor = 0;
+		number_read_successfully = sscanf(number_string, "%d", &all_numbers[all_number_incrementor]);
 		if(number_read_successfully != 1) {
 			all_numbers_read_successfully = false;
 			break;
 		}
 
-		if(all_numbers[k] >= 0) {
-			positive_numbers[j] = all_numbers[k];
+		if(all_numbers[all_number_incrementor] >= 0) {
+			positive_numbers[positive_number_incrementor] = all_numbers[all_number_incrementor];
 			positive_number_count++;
-			j++;
+			positive_number_incrementor++;
 		} else {
-			negative_numbers[m] = all_numbers[k];
+			negative_numbers[negative_number_incrementor] = all_numbers[all_number_incrementor];
 			negative_number_count++;
-			m++;
+			negative_number_incrementor++;
 		}
-		k++;
+		all_number_incrementor++;
 	}
 
 	/* input validation */
 
-	/* calculate positive and negative number totals */
-	int size_of_array = sizeof(positive_numbers)/sizeof(int);
 	/* output results */
 	printf("\nThere are %d positive numbers and %d negative numbers.\n", positive_number_count, negative_number_count);
 	printf("Positive numbers:\n");
-	for(int i = 0; i < j; i++) {
+	for(int i = 0; i < positive_number_incrementor; i++) {
 		printf("%d\n", positive_numbers[i]);
 	}
 
 	printf("Negative numbers:\n");
-	for(int i = 0; i < m; i++) {
+	for(int i = 0; i < negative_number_incrementor; i++) {
 		printf("%d\n", negative_numbers[i]);
 	}
 }
