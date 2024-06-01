4bdc945b63cb5561dc6fa1ba5cae541262dd222c
Steven Nava <trey.nobles@gmail.com>
2024-06-01 00:33:50 -0400

    Added program for calculating total resistance in ohms for entered in resistor value

--- Practical_C/trivial_programs/calculate_total_resistance_prompt_resistor.c
+++ Practical_C/trivial_programs/calculate_total_resistance_prompt_resistor.c
@@ -16,6 +16,39 @@
 #include <string.h>
 
 int main() {
+	/* variable declaration */
+	char line[50],
+	invalid_characters[50];
+	int number_of_resistors,
+	number_read_in_successfully,
+	numerator,
+	total_numerator,
+	denominator,
+	first_denominator,
+	temporary_value;
+	double resistor_decimal,
+	total_resistor_value,
+	total_resistance;
+
+	/* prompt and read in number of resistors */
+	printf("Enter the total number of resistors: ");
+	fgets(line, sizeof(line), stdin);
+	sscanf(line, "%d", &number_of_resistors);
+
+	/* prompt for all resistors */
+	for(int i = 0; i < number_of_resistors; i++) {
+		printf("\nEnter the resistor denominator: ");
+		fgets(line, sizeof(line), stdin);
+		sscanf(line, "%d", &denominator);
+
+		/* convert resistor to decimal value and add to totals */
+		resistor_decimal = (double)1 / (double)denominator;
+		total_resistor_value += resistor_decimal;
+	}
+
+	total_resistance = (double)1 / total_resistor_value;
+
+	printf("\nThe total resistance for the %d resistors you entered in is %.2lf ohms.\n", number_of_resistors, total_resistance);
 
 	return(0);
 }
