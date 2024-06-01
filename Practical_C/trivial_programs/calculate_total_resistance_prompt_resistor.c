dbb52486f7e379935484e0148c8148b4f64fed41
Steven Nava <trey.nobles@gmail.com>
2024-05-31 23:57:22 -0400

    Added program for calculating total resistance by prompting for a number of resistors

---
+++ Practical_C/trivial_programs/calculate_total_resistance_prompt_resistor.c
@@ -0,0 +1,21 @@
+/********************************************************************************
+ *										*
+ *	Name: 	 Calculate Total Resistance by Prompts				*
+ *										*
+ *	Author:	 Steven Nava							*
+ *										*
+ *	Purpose: Calculate total resistance by prompting for each resistor 	*
+ *		 in parallel.							*
+ *		 								*
+ *	Usage:	 Run program. Enter in resistors for the number of resistors	*
+ *		 prompted. Total resistance will be calculated and output.	*
+ *		 								*
+ ********************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+	return(0);
+}
