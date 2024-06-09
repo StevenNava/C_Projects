2e8d37271b273c67c8b75d93a8ebb90f436ae54e
Steven Nava <trey.nobles@gmail.com>
2024-06-08 21:33:49 -0400

    Correced issue in averaging program with hard coded value

--- Practical_C/trivial_programs/average_given_number_of_numbers.c
+++ Practical_C/trivial_programs/average_given_number_of_numbers.c
@@ -1,15 +1,15 @@
 /************************************************************************
- *									*
- *	Name:	 Average Given Number of Numbers			*
- *									*
- *	Author:	 Steven Nava						*
- *									*
- *	Purpose: Averages a given number of numbers entered in by user.	*
- *									*
- *	Usage:	 Run program. Enter the number of numbers to be		*
- *		 averaged. Enter the numbers -- these will be totalled	*
- *		 and then averaged and output by the program.		*
- *		 							*
+ *									                                    *
+ *	Name:	 Average Given Number of Numbers			                *
+ *									                                    *
+ *	Author:	 Steven Nava						                        *
+ *									                                    *
+ *	Purpose: Averages a given number of numbers entered in by user.	    *
+ *									                                    *
+ *	Usage:	 Run program. Enter the number of numbers to be		        *
+ *		 averaged. Enter the numbers -- these will be totalled	        *
+ *		 and then averaged and output by the program.		            *
+ *		 							                                    *
  ************************************************************************/
 
 #include <stdio.h>
@@ -18,7 +18,7 @@
 int main() {
 	/* variable declaration */
 	char line[50];
-	int number_of_numbers_to_average, 
+	int number_of_numbers_to_average,
 	whole_numbers,
 	partial_numbers,
 	whole_numbers_total = 0,
@@ -34,7 +34,7 @@ int main() {
 			printf("\nA valid number of numbers to be averaged was not entered in. Please enter in a valid number greater than 2.\n");
 			continue;
 		}
-		while(i <= 3) {
+		while(i <= number_of_numbers_to_average) {
 			/* read in variables and input validation */
 			printf("\nEnter number %d: ", i);
 			fgets(line, sizeof(line), stdin);
@@ -51,7 +51,7 @@ int main() {
 	printf("\nWhole Numbers: %d   Partial Numbers: %d \n", whole_numbers_total, partial_numbers_total);
 	/* calculate average and output */
 	average = (double)((whole_numbers_total * 100) + partial_numbers_total) / (double)(number_of_numbers_to_average * 100);
-	
+
 	printf("\nThe average of the %d numbers you entered in is %.2lf.\n", number_of_numbers_to_average, average);
 	return 0;
 }
