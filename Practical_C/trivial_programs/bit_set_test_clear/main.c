9177b9bbec6387b6f9769173b73f1b0a93cb6bbb
Steven Nava <trey.nobles@gmail.com>
2024-06-15 00:05:30 -0400

    Added section and comments about setting position and pushing with int position as opposed to hex'

--- Practical_C/trivial_programs/bit_set_test_clear/main.c
+++ Practical_C/trivial_programs/bit_set_test_clear/main.c
@@ -15,18 +15,19 @@
 #include <stdio.h>
 
 /* Sets bit left # from position as on */
-#define SET_BIT_LEFT(x, bit_move) (x) |= (0x80 >>(bit_move))
+/* Can use hex position or integer position converted to binary to define starting point */
+#define SET_BIT_LEFT(x, bit_move) (x) |= (128 >>(bit_move))
 
 /* Sets bit right # from position as on */
-#define SET_BIT_RIGHT(x, bit_move) (x) |= (0x80 <<(bit_move))
+#define SET_BIT_RIGHT(x, bit_move) (x) |= (1 <<(bit_move))
 
 int main(void) {
     /* int has # positions */
     int byte;
     for(int i = 0; i < 8; i++) {
         /* odd is if(i == 1 || i % 2 != 0) { */
-        if(i == 1 || i % 2 != 0) {
-            SET_BIT_RIGHT(byte, i);
+        if(i == 0 || i % 2 == 0) {
+            SET_BIT_LEFT(byte, i);
             printf("\n%d\n", byte);
         }
     }
