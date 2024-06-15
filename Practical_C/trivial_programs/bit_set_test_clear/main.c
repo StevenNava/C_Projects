518522d1ae18896d500f7b432682b311ca640620
Steven Nava <trey.nobles@gmail.com>
2024-06-14 23:49:45 -0400

    Modified bitwise operations to work and added comments describing what is being done on the set bit operation

--- Practical_C/trivial_programs/bit_set_test_clear/main.c
+++ Practical_C/trivial_programs/bit_set_test_clear/main.c
@@ -1,15 +1,34 @@
+/************************************************************
+ *                                                          *
+ *  Name:    Perform Bitwise Operations                     *
+ *                                                          *
+ *  Author:  Steven Nava                                    *
+ *                                                          *
+ *  Purpose: Performs bitwise operations, by turning on/off *
+ *           specific bits.                                 *
+ *                                                          *
+ *  Usage:   Run program and it will toggle on bits by an   *
+ *           even number of bits every run and output       *
+ *           the result.                                    *
+ *                                                          *
+ ************************************************************/
 #include <stdio.h>
 
-#define SET_BIT(x) (x) |= (0x80 >>((x)%8))
+/* Sets bit left # from position as on */
+#define SET_BIT_LEFT(x, bit_move) (x) |= (0x80 >>(bit_move))
+
+/* Sets bit right # from position as on */
+#define SET_BIT_RIGHT(x, bit_move) (x) |= (0x80 <<(bit_move))
 
 int main(void) {
+    /* int has # positions */
     int byte;
-
     for(int i = 0; i < 8; i++) {
-        if(i == 0 || i % 2 == 0) {
-            SET_BIT(byte);
+        /* odd is if(i == 1 || i % 2 != 0) { */
+        if(i == 1 || i % 2 != 0) {
+            SET_BIT_RIGHT(byte, i);
+            printf("\n%d\n", byte);
         }
-        printf("\n%d\n", byte);
     }
 
     return 0;
