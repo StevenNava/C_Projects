84b831a2c21aac2c30b9dc54b9c596c9fcc51883
Steven Nava <trey.nobles@gmail.com>
2024-06-17 23:49:55 -0400

    Added notes to print square bitmap program

--- Practical_C/trivial_programs/bit_set_bitmap_square/main.c
+++ Practical_C/trivial_programs/bit_set_bitmap_square/main.c
@@ -1,9 +1,27 @@
+/************************************************
+ *                                              *
+ *  Name:    Print 10 x 10 Bitmap Square        *
+ *                                              *
+ *  Author:  Steven Nava                        *
+ *                                              *
+ *  Purpose: Prints a 10 x 10 bitmapped square. *
+ *                                              *
+ *  Usage:   Run program. 10 x 10 square will   *
+ *           print out of '*' characters. To    *
+ *           change the size, change X_SIZE,    *
+ *           Y_SIZE, and the if statement for   *
+ *           i and j.                           *
+ *                                              *
+ ************************************************/
+
 #include <stdio.h>
 #define X_SIZE 16
 #define Y_SIZE 16
 
+/* define a graphics array in bytes (X_SIZE / 8) by bits */
 char graphics[X_SIZE/8][Y_SIZE]; // graphics array
 
+/* set bit */
 #define SET_BIT(x,y) graphics[(x)/8][y] |= (0x80 >> ((x)%8))
 
 void print_graphics() {
@@ -14,6 +32,7 @@ void print_graphics() {
     for(y = 0; y < Y_SIZE; y++) {
         for(x = 0; x < X_SIZE / 8; x++) {
             for(bit = 0x80; bit > 0; bit = (bit >> 1)) {
+                /* if bit is set */
                 if((graphics[x][y] & bit) != 0) {
                     printf("*");
                 } else {
@@ -21,6 +40,7 @@ void print_graphics() {
                 }
             }
         }
+        /* print character to move to next 'y' line */
         printf("\n");
     }
 }
@@ -29,6 +49,7 @@ int main(void) {
 
     for(int i = 0; i < X_SIZE; i++) {
         for(int j = 0; j < Y_SIZE; j++) {
+            /* check for nodes on centered 10 x 10 square */
             if(((i == 4 || i == 13) && (j <= 13 && j >= 4)) || ((i >= 4 && i <= 13) && (j == 13 || j == 4))) {
                 SET_BIT(i, j);
             }
