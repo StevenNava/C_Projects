ed80793087039cd0cb500c3887f6159af4895d07
Steven Nava <trey.nobles@gmail.com>
2024-06-20 22:48:34 -0400

    Used shift and & bitwise operator with a loop to split long int into four smaller ints

--- Practical_C/trivial_programs/bit_set_split_long_int_into_four/bit_set_split_long_int_into_four.c
+++ Practical_C/trivial_programs/bit_set_split_long_int_into_four/bit_set_split_long_int_into_four.c
@@ -16,19 +16,18 @@
 
 #include <stdio.h>
 
-#define SET_BIT_LEFT(x, bit_move) (x) |= (0x80 >> (bit_move))
+#define SET_BIT_RIGHT(x, bit_move) (x) |= (0x01 << (bit_move))
 #define SPLIT_BIT(a, b, c, d, l_int) 	long j = l_int; \
-					for(int i = 0, bit = 0x80000000; i < 32; i++) { \
-						bit = (bit >> 1); \
-						if(((j << i) & bit) != 0) { \
+					for(int i = 0; i < 32; i++) { \
+						if(((j >> i) & 0x01) != 0) { \
 							if(i <= 7) { \
-								SET_BIT_LEFT(a, i); \
+								SET_BIT_RIGHT(a, i); \
 							} else if(i <= 15) { \
-								SET_BIT_LEFT(b, i - 8); \
+								SET_BIT_RIGHT(b, i - 8); \
 							} else if (i <= 23) {  \
-								SET_BIT_LEFT(c, i - 16); \
+								SET_BIT_RIGHT(c, i - 16); \
 							} else { \
-								SET_BIT_LEFT(d, i - 24); \
+								SET_BIT_RIGHT(d, i - 24); \
 							} \
 						} \
 					} 
@@ -38,7 +37,7 @@ int main() {
 	int a, b, c, d;
 	SPLIT_BIT(a, b, c, d, number);
 
-	printf("\n%d\n%d\n%d\n%d\n", a, b, c, d);
+	printf("\n%d\n%d\n%d\n%d\n", d, c, b, a);
 	return 0;
 }
 
