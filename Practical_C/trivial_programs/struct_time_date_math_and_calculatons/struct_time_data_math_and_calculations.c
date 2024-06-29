8ae08e7e40be7f1bf918be8a51b78376248a519d
Steven Nava <trey.nobles@gmail.com>
2024-06-28 23:50:17 -0400

    Created program to do some date time math

---
+++ Practical_C/trivial_programs/struct_time_date_math_and_calculatons/struct_time_data_math_and_calculations.c
@@ -0,0 +1,28 @@
+/****************************************************************
+ *								*
+ *	Name: 	 Time and Date Math				*
+ *								*
+ *	Author:	 Steven Nava					*
+ *								*
+ *	Purpose: Performs time and date math between two given	*
+ *		 dates and times.				*
+ *		 						*
+ *	Usage:	 Run program. Enter a valid date. Enter a valid	*
+ *		 time. Enter a second date. Enter a second 	*
+ *		 time. Program will perform math and output	*
+ *		 difference in minutes between the two dates.	*
+ *		 						*
+ ****************************************************************/
+
+#include <stdio.h>
+
+struct date_time {
+	char date[12];
+	char time[10];
+	int number_of_minutes;
+};
+
+int main() {
+	struct date_time dates[2];
+	return 0;
+}
